2259c4c0af125743f817e5d239b284567dbfbeb1
ChaseRensberger <chasehrensberger@gmail.com>
2024-01-31 16:42:14 -0500

    begin layout construction

--- calculator/ContentView.swift
+++ calculator/ContentView.swift
@@ -9,15 +9,63 @@ import SwiftUI
 import RealityKit
 import RealityKitContent
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Model3D(named: "Scene", bundle: realityKitContentBundle)
-                .padding(.bottom, 50)
+enum CalculatorButton: String {
+    case zero
+    case one
+    case two
+    case three
+    case four
+    case five
+    case six
+    case seven
+    case eight
+    case nine
+    case decimal
+    case equals
+    case plus
+    case minus
+    case multiply
+    case divide
+    case percent
+    case toggleSign
+    case clear
+}
 
-            Text("Hello, world!")
+struct ContentView: View {
+    
+    let buttons: [[CalculatorButton]] = [
+        [.seven, .eight, .nine]
+    ]
+    
+    var body: some View {
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+            VStack {
+                HStack {
+                    Spacer()
+                    Text("0")
+                        .font(.system(size: 64))
+                        .bold()
+                        .foregroundColor(.white)
+                }
+                .padding()
+
+                ForEach(buttons, id: \.self) { row in
+                    ForEach(row, id: \.self) { button in
+                        Button(action: {
+                            
+                        }, label: {
+                            Text(button.rawValue)
+                                .frame(width: 70, height: 70)
+                                .background(Color.orange)
+                                .foregroundColor(.white)
+                                .cornerRadius(35)
+                        })
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
