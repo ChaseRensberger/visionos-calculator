f2d87d3f1895cc8e72fbf04490f5480ddae70ceb
ChaseRensberger <chasehrensberger@gmail.com>
2024-01-30 20:23:08 -0500

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Chase Rensberger on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
